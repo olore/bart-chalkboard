38281b2df26561e70eb08eb731dadee3c2330e7a
Brian Olore <brian@olore.net>
2020-09-05 09:09:09 -0400

    use chalkboard font, add thanks in README

---
+++ README.md
@@ -0,0 +1,4 @@
+Thanks
+* http://www.ranzey.com/generators/bart/index.html
+* https://github.com/janl/mustache.js/
+* https://www.fontspace.com/pencilpete-font-f8223
--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const fs = require('fs');
 const Mustache = require("mustache");
 
-const html = fs.readFileSync("template.mustache").toString();
-const data = {
-  text: "I will go to bed on time"
+const repeat = (text, lineCount) => {
+  return [...Array(lineCount).keys()].map(() => text);
 }
 
-const output = Mustache.render(html, data);
+const html = fs.readFileSync("template.mustache").toString();
+const text = "I will not chew gum in class any more. Cool beans?";
 
+let config = { 
+  lineHeight: "30px",
+  lineCount: 12,
+};
+
+console.log(text.length);
+if (text.length <= 15) {
+  config.fontSize = "40px";
+} else if (text.length <= 37) {
+  config.fontSize = "30px";
+} else if (text.length <= 55) {
+  config.fontSize = "22px";
+} else {
+  config.fontSize = "12px";
+}
+
+const data = {
+  fontSize: config.fontSize,
+  lineHeight: config.lineHeight,
+  text: repeat(text, config.lineCount)
+}
+
+
+const output = Mustache.render(html, data);
 fs.writeFileSync("output.html", output);
+
