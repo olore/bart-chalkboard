639559545af003e3067e9cb375b03ad3e7a67e23
Brian Olore <brian@olore.net>
2020-09-12 09:20:12 -0400

    try clipping the screenshot

--- puppeteer.js
+++ puppeteer.js
@@ -7,12 +7,12 @@ const screenshot = async (text) => {
   await page.goto('https://bart.olore.net?s=' + text, { waitUntil: 'networkidle2' });
   await page.screenshot({
     path: `public/pics/${kebabCase(text)}.png`,
-    // clip: {
-    //   x: 0,
-    //   y: 0,
-    //   width: 100,
-    //   height: 100,
-    // }
+    clip: {
+      x: 0,
+      y: 0,
+      width: 800,
+      height: 600,
+    }
   });
   await browser.close();
 };
