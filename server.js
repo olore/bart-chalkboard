0cb50eb595e789cd9d21c6363c8a55d077376bce
Brian Olore <brian@olore.net>
2020-09-12 02:47:18 +0000

    add slack response with dummy pic for now

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const { generate } = require("./index");
 const app = express()
 const port = 8000
 
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
+  console.log("From Web: " + req.query.s);
   res.send(generate(req.query.s));
+})
+
+app.post('/slack', (req, res) => {
+  console.log("From Slack: " + req.body.text);
+  res.json({
+    "response_type": "in_channel",
+    "text": generate(req.body.text),
+    "attachments": [{
+      "image_url": "https://raw.githubusercontent.com/olore/bart-chalkboard/master/public/bart-chalkboard.png"
+    }]
+  });
 })
 
 app.listen(port, () => {
