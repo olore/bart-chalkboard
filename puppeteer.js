2c11b1e2e249df5384bea8624db2f9f997cf9d1c
Brian Olore <brian@olore.net>
2020-09-19 19:54:50 -0400

    add logging and send respond with error if no message for chalkboard

--- puppeteer.js
+++ puppeteer.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 const screenshot = async (text) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://bart.olore.net?s=' + text, { waitUntil: 'networkidle2' });
+  await page.goto('https://bart.olore.net?slack=true&s=' + text, { waitUntil: 'networkidle2' });
   await page.screenshot({
     path: `public/pics/${kebabCase(text)}.png`,
     clip: {
--- server.js
+++ server.js
@@ -10,23 +10,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  console.log("From Web: " + req.query.s);
-  res.send(generate(req.query.s));
+  console.log(`Request from ${req.ip}`);
+
+  let text = req.query && req.query.s;
+  if (text) {
+    if (!req.query.slack) {
+      console.log(`From Web: ${text}`);
+    }
+    res.send(generate(text));
+  } else {
+    res.status(400).send("Invalid request");
+  }
 })
 
 app.post('/slack', async (req, res) => {
-  console.log("From Slack: " + req.body.text);
+  let text = req.query && req.query.s;
+  console.log(`From Slack message: ${text}`);
+  console.log(`From Slack: ${req.body.team_domain} - ${req.body.user_name}`);
   const url = `https://bart.olore.net/pics/${kebabCase(req.body.text)}.png`;
-  console.log(url);
 
-  await screenshot(req.body.text);
+  await screenshot(text);
 
   res.json({
     "response_type": "in_channel",
-    //"text": req.body.text,
+    "text": text,
     "attachments": [{
       "image_url": url
     }]
+
+    // Maybe use this to respond in thread? - https://api.slack.com/messaging/sending#threading
+    // "channel": "YOUR_CHANNEL_ID", // channel_id
+    // "thread_ts": "PARENT_MESSAGE_TS",
+
   });
 })
 
